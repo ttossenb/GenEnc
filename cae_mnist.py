d6da1138d80cfde73122a82219aca418c3e23c33
ttossenb <43469325+ttossenb@users.noreply.github.com>
2018-11-03 05:54:55 +0100

    Add files via upload

---
+++ cae_mnist.py
@@ -0,0 +1,67 @@
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape
+from keras.models import Model
+from keras import backend as K
+
+latent_dim=6
+
+input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
+
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+x = Flatten()(x)
+encoded = Dense(latent_dim, activation='relu')(x)
+
+x = Dense(4*4*8, activation='relu')(encoded)
+x = Reshape((4, 4, 8))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+encoder = Model(input_img, encoded)
+
+from keras.datasets import mnist
+import numpy as np
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+np.save("latent_points", encoder.predict(x_train))
+
+# serialize model to JSON
+encoder_json = encoder.to_json()
+with open("encoder.json", "w") as json_file:
+    json_file.write(encoder_json)
+# serialize weights to HDF5
+encoder.save_weights("encoder.h5")
+print("Saved encoder to disk")
+
+# serialize model to JSON
+autoencoder_json = autoencoder.to_json()
+with open("autoencoder.json", "w") as json_file:
+    json_file.write(autoencoder_json)
+# serialize weights to HDF5
+autoencoder.save_weights("autoencoder.h5")
+print("Saved autoencoder to disk")
